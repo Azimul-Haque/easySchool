e75df1367beb84b6a2fd8716e773e9c5f5c4f98e
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2017-10-31 19:56:37 +0600

    School Settings Progress v 1.0.1

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -14,41 +14,43 @@
 @section('content')
   {!! Form::model($settings, ['method' => 'PATCH','route' => ['settings.update', $settings->id]]) !!}
   <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Current Session</strong>
-                <select class="form-control" name="currentsession">
-                @php
-                  $y = 2012;
-                  for($y; $y<=2038; $y++) {
-                @endphp
-                    <option value="{{ $y }}" @if($y == $settings->currentsession) {{ 'selected' }} @endif>{{ $y }}</option>
-                @php
-                  }
-                @endphp
-                </select>
-            </div>
+    <div class="col-md-6">
+      <div class="well">
+        <div class="form-group">
+          <strong>Current Academic Session</strong>
+          <select class="form-control" name="currentsession">
+          @php
+            $y = 2012;
+            for($y; $y<=2038; $y++) {
+          @endphp
+              <option value="{{ $y }}" @if($y == $settings->currentsession) {{ 'selected' }} @endif>{{ $y }}</option>
+          @php
+            }
+          @endphp
+          </select>
         </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Classes:</strong>
-                <br/>
-                @php
-                $classes = explode(',', $settings->classes);
-                @endphp
-                @for($clss = -2;$clss<=10;$clss++)
-                  <label style="margin-right: 40px;">
-                  <input type="checkbox" name="classes[]" value="{{ $clss }}" class="classes" 
-                  @if(in_array($clss, $classes)) {{ 'checked' }} @endif
-                  /> {{ $clss }}
-                  </label>
-                @endfor
-            </div>    
+        <div class="form-group">
+          <strong>Classes:</strong>
+          <br/>
+          @php
+          $classes = explode(',', $settings->classes);
+          @endphp
+          @for($clss = -2;$clss<=10;$clss++)
+            <label style="margin-right: 40px;">
+            <input type="checkbox" name="classes[]" value="{{ $clss }}" class="classes" 
+            @if(in_array($clss, $classes)) {{ 'checked' }} @endif
+            /> {{ $clss }}
+            </label>
+          @endfor
+        </div> 
+        <div class="form-group">
+            <strong>Name:</strong>
+            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
         </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+    
         <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
+      </div>
+    </div>
   </div>
   {!! Form::close() !!}
 @stop
