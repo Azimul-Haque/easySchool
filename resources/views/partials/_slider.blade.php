a9aaabc9caddd311382dce68a38805bd8cb5737c
Orbachin Ujbuk <orbachinujubk@gmail.com>
2018-12-13 04:12:53 +0600

    Slider fadeInUp

--- resources/views/partials/_slider.blade.php
+++ resources/views/partials/_slider.blade.php
@@ -1,4 +1,4 @@
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<div id="myCarousel" class="carousel slide wow fadeInUp" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
