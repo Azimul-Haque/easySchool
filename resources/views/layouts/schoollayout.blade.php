ea2423a84b862c64925bd51a6f753a9fb196e6ce
Orbachin Ujbuk <orbachinujubk@gmail.com>
2018-12-14 00:06:27 +0600

    Tawk Intigration!

--- resources/views/layouts/schoollayout.blade.php
+++ resources/views/layouts/schoollayout.blade.php
@@ -1,6 +1,3 @@
-@php 
-    ob_start("ob_gzhandler");  
-@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -40,6 +37,19 @@
     <link rel="stylesheet" href="{{ asset('vendor/adminlte/dist/css/AdminLTE.min.css') }}">
     @yield('css')
 </head>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5c12972c7a79fc1bddf0d445/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 <body id="app-layout">
     <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR9XJZP"
