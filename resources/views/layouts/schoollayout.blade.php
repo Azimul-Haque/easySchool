232ae98708b98743a7ff2cf019fc3d985024177a
Orbachin Ujbuk <orbachinujubk@gmail.com>
2018-12-13 22:09:18 +0600

    Gzip 2.0.0

--- resources/views/layouts/schoollayout.blade.php
+++ resources/views/layouts/schoollayout.blade.php
@@ -1,5 +1,10 @@
-<?php if (substr_count($_SERVER[‘HTTP_ACCEPT_ENCODING’], ‘gzip’))
-ob_start(“ob_gzhandler”); else ob_start(); ?>
+@php 
+    if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+        ob_start("ob_gzhandler"); 
+    } else {
+        ob_start();
+    } 
+@endphp
 <!DOCTYPE html>
 <html lang="en">
 <head>
