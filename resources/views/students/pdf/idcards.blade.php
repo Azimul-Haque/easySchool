77d528327d3c2fca9228abd7d9a19e9efc763ca5
orbachinujbuk <orbachinujbuk@gmail.com>
2019-07-10 02:32:35 +0600

    ID Card Update

--- resources/views/students/pdf/idcards.blade.php
+++ resources/views/students/pdf/idcards.blade.php
@@ -67,8 +67,8 @@
     @foreach($students as $student)
       <div style="height: 290px; width: 200px; background: #183172; border: 1px solid #183172; float: left; margin-bottom: 15px;">
         <div style="height: 290px; width: 200px; background: #fff; border-radius: 50px 0px 50px 0px; padding: 10px 5px 10px 5px;">
-          <center><p style="font-size: 14px; margin-top: 10px; text-align: center; margin: auto;"><b>{{ Auth::user()->school->name }}</b></p></center>
-          <p style="font-size: 8px; font-family: times; padding: 2px; margin-top: 1px; text-align: center;">
+          <center><p style="font-size: 15px; margin-top: 10px; text-align: center; margin: auto;"><b>{{ Auth::user()->school->name }}</b></p></center>
+          <p style="font-size: 9px; font-family: times; padding: 0px; margin-top: 1px; text-align: center;">
             <b>{{ Auth::user()->school->upazilla }}, {{ Auth::user()->school->district }}</b>
           </p>
           <table>
