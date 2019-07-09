04e3152026f6b04526b5e1b23fc6c305c525d268
orbachinujbuk <orbachinujbuk@gmail.com>
2019-07-10 02:29:05 +0600

    Update ID Card Design! :/

--- resources/views/students/pdf/idcards.blade.php
+++ resources/views/students/pdf/idcards.blade.php
@@ -24,10 +24,19 @@
     border: 0px solid white !important;
   }
   .thumb1 { 
-    width: 80px !important;
-    height: 80px !important;
-    margin: auto;
+    width: 100%;
+    height: 95px !important;
+    margin: 5px;
     margin-top: 10px;
+    float: left;
+
+    background: red;
+  }
+  .thumb2 { 
+    width: 60px;
+    height: 60px !important;
+    /*margin: 5px;*/
+    float: left;
   }
   
 
@@ -38,6 +47,13 @@
   .dataTable tr td, .dataTable tr th {
       border: 1px solid lightgreen;
   }
+  .data_td_parents_left {
+    padding-left: 5px;
+    font-size: 12.5px;
+  }
+  .data_td_parents_right {
+    font-size: 12.5px;
+  }
   @page {
     header: page-header;
     footer: page-footer;
@@ -45,111 +61,90 @@
   </style>
 </head>
 <body>
-  <table class="maintable">
-    <tr>
     @php
       $counter = 1;
     @endphp
     @foreach($students as $student)
-      <td style="padding: 11px;">
-        <table class="containerTable" style="width: 100%; background: lightgreen; border: 2px solid #696969; width: 200px; height: 310px;">
-          <tr>
-            <td>
-              <table style="background: #FAFA6C;">
-                <tr>
-                  <td>
-                    <span style="font-size: 15px; padding: 5px; margin-top: 10px; font-family: times"><center><b><!-- <img src="logo.jpg" style="max-height: 40px;"> --> {{ Auth::user()->school->name }}</b></center></span>
-                    <span style="font-size: 8px; font-family: times; padding: 2px; margin-top: 1px;">
-                      <center>
-                        <b>{{ Auth::user()->school->upazilla }}, {{ Auth::user()->school->district }}</b>
-                      </center>
-                    </span><br/>
-                    <table style="width: 75%; border-collapse: collapse; border-radius: 1em; overflow: hidden; margin-bottom: 15px;">
-                      <tr>
-                        <td style="display: inline-block; font-size: 14px; background: grey; color:white; border-radius: 5px; font-family: Helvetica; margin: auto; padding: 3px; margin-top: 1px;">
-                          <center><b>STUDENT ID CARD</b></center>
-                        </td>
-                      </tr>
-                    </table>
+      <div style="height: 290px; width: 200px; background: #183172; border: 1px solid #183172; float: left; margin-bottom: 15px;">
+        <div style="height: 290px; width: 200px; background: #fff; border-radius: 50px 0px 50px 0px; padding: 10px 5px 10px 5px;">
+          <center><p style="font-size: 14px; margin-top: 10px; text-align: center; margin: auto;"><b>{{ Auth::user()->school->name }}</b></p></center>
+          <p style="font-size: 8px; font-family: times; padding: 2px; margin-top: 1px; text-align: center;">
+            <b>{{ Auth::user()->school->upazilla }}, {{ Auth::user()->school->district }}</b>
+          </p>
+          <table>
+            <tr>
+              <td width="60%" style="padding: 5px;">
+                @if($student->image == '')
+                    <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:95px; border: 2px solid #000;" alt="N/A">
+                @else
+                  @if(file_exists(public_path('images/admission-images/'.$student->image))) 
+                    <img src="{{ public_path('images/admission-images/'.$student->image) }}" style="height:95px; border: 2px solid #000;" alt="N/A">
+                  @else
+                    <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:95px; border: 2px solid #000;" alt="N/A">
+                  @endif
+                @endif
+              </td>
+              <td style="padding: 5px;">
+                @if(Auth::user()->school->monogram == '')
+                    {{-- <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:60px;" alt="N/A"> --}}
+                @else
+                  @if(file_exists(public_path('images/schools/monograms/'.Auth::user()->school->monogram))) 
+                    <img src="{{ public_path('images/schools/monograms/'.Auth::user()->school->monogram) }}" style="height:60px;" alt="N/A">
+                  @else
+                    {{-- <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:60px;" alt="N/A"> --}}
+                  @endif
+                @endif
+              </td>
+            </tr>
+          </table>
 
-                    <div class="thumb1">
-                      @if($student->image == '')
-                          <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:80px; border: 1px solid green;" alt="N/A">
-                      @else
-                        @if(file_exists(public_path('images/admission-images/'.$student->image))) 
-                          <img src="{{ public_path('images/admission-images/'.$student->image) }}" style="height:80px; border: 1px solid green;" alt="N/A">
-                        @else
-                          <img src="{{ public_path('images/dummy_student.jpg') }}" style="height:80px; border: 1px solid green;" alt="N/A">
-                        @endif
-                      @endif
-                    </div><br/>
+          <p style="font-size: 11.5px; font-weight: bold; color: navy; margin: 5px; border-bottom: 1px solid navy; text-align: center;">
+            {{ $student->name }}
+          </p>
 
-                    <center>
-                      @if(strlen($student->name) > 25)
-                        <div style="font-size: 10px; font-weight: bold; color: navy; background: #FAFA6C; margin-bottom: 5px;">
-                          {{ $student->name }}
-                        </div>
-                      @else
-                        <div style="font-size: 13px; font-weight: bold; color: navy; background: #FAFA6C; margin-bottom: 5px;">
-                          {{ $student->name }}
-                        </div>
-                      @endif
-                      <hr/>
-                    </center>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <table border="0" style="width: 100%; margin-top: 10px;" class="dataTable">
-                <tr>
-                  <td width="25%" style="width: 25%;"><span style="color: lightgreen;">...</span>Father</td>
-                  <td style="">: {{ ucwords(strtolower($student->father)) }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 25%;"><span style="color: lightgreen;">...</span>Mother</td>
-                  <td style="">: {{ ucwords(strtolower($student->mother)) }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 25%;"><span style="color: lightgreen;">...</span>Village</td>
-                  <td style="">: {{ ucwords(strtolower($student->village)) }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 25%;"><span style="color: lightgreen;">...</span>DoB</td>
-                  <td>: {{ date('d-m-Y', strtotime($student->dob)) }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 25%;"><span style="color: lightgreen;">...</span>Contact</td>
-                  <td>: {{ $student->contact }}</td>
-                </tr>
-                <tr>
-                  <td style="width: 25%;"><span style="color: lightgreen;">...</span>St. ID</td>
-                  <td>: <span style="font-size: 13px; font-weight: bold; color: navy;">{{ $student->student_id }}</span></td>
-                </tr>
-              </table>
-              <br/>
-            </td>
-          </tr>
-        </table>
-      </td>
+          <table border="0" >
+            <tr>
+              <td class="data_td_parents_left">Father</td>
+              <td class="data_td_parents_right">: {{ ucwords(strtolower($student->father)) }}</td>
+            </tr>
+            <tr>
+              <td class="data_td_parents_left">Mother</td>
+              <td class="data_td_parents_right">: {{ ucwords(strtolower($student->mother)) }}</td>
+            </tr>
+            <tr>
+              <td class="data_td_parents_left">Village</td>
+              <td class="data_td_parents_right">: {{ ucwords(strtolower($student->village)) }}</td>
+            </tr>
+            <tr>
+              <td class="data_td_parents_left">DoB</td>
+              <td class="data_td_parents_right">: {{ date('d-m-Y', strtotime($student->dob)) }}</td>
+            </tr>
+            <tr>
+              <td class="data_td_parents_left">Contact</td>
+              <td class="data_td_parents_right">: {{ $student->contact }}</td>
+            </tr>
+            <tr>
+              <td class="data_td_parents_left">St. ID</td>
+              <td class="data_td_parents_right">: <span style="font-size: 14px; font-weight: bold; color: navy;">{{ $student->student_id }}</span></td>
+            </tr>
+          </table>
+          
+        </div>
+      </div>
+      
+    @if($counter%3 != 0)
+      <div style="height: 290px; width: 15px; float: left; padding: 0px;"></div>
+    @endif
     @if($counter%3 == 0)
-      </tr> <tr>
+      <br/>
     @endif
     @if($counter%9 == 0)
-     </tr>
-    </table>
      <pagebreak></pagebreak>
-    <table class="maintable">
-      <tr>
     @endif
     @php
       $counter++;
     @endphp
     @endforeach
-    </tr>
-  </table>
 
   <htmlpagefooter name="page-footer">
     <small style="font-family: Calibri; color: #6D6E6A;">Powered by EasySchool.XYZ</small>
