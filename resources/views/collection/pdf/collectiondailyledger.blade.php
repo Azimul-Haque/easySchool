666561fa04bb0b95ba5fd77fb50f0aede6d275dc
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2022-03-25 01:41:34 +0600

    Commit Fri 03/25/2022  1:41:34.14 20315

--- resources/views/collection/pdf/collectiondailyledger.blade.php
+++ resources/views/collection/pdf/collectiondailyledger.blade.php
@@ -108,7 +108,7 @@
                     <td align="center">{{ date('d-m-y', strtotime($datekey)) }}</td>
                     <td align="center">{{ $sectioncollections[0]->class }}</td>
                     <td align="center">{{ english_section_short(Auth::user()->school->section_type, $sectioncollections[0]->class, $sectioncollections[0]->section) }}</td>
-                    <td>{{ $sectioncollections[0]->receipt_no }} {{ count($sectioncollections) }}</td>
+                    <td>{{ $sectioncollections[0]->receipt_no }} হতে {{ $sectioncollections[count($sectioncollections)-1] }}</td>
                     <td align="center">
                         @php
                             $total_single_section_single_sector_fee = 0;
