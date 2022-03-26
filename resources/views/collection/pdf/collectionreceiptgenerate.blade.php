f3d4ce067c4dde182d95585244e8e6277bcd604f
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2022-03-26 18:08:39 +0600

    Commit Sat 03/26/2022 18:08:39.35 25990

--- resources/views/collection/pdf/collectionreceiptgenerate.blade.php
+++ resources/views/collection/pdf/collectionreceiptgenerate.blade.php
@@ -38,12 +38,12 @@
                                 <center>
                                     <b>{{ Auth::user()->school->name_bangla }}</b><br/>
                                     <span style="font-size: 14px;">
-                                    স্থাপিতঃ {{ bangla(Auth::user()->school->address) }} ইংরেজি | ইআইআইএনঃ {{ bangla(Auth::user()->school->eiin) }}<br/>
+                                    স্থাপিতঃ {{ bangla(Auth::user()->school->address) }} ইংরেজি | ইআইআইএনঃ {{ bangla(Auth::user()->school->eiin) }}
+                                    </span><br/>
                                     <span style="font-size: 14px;">
                                         <b>{{ bangla(date('F d, Y', strtotime($data[0]))) }} - {{ bangla(date('F d, Y', strtotime($data[1]))) }}</b>
                                     </span><br/>
                                     <span style="font-size: 14px;"><u>দৈনিক খতিয়ান</u></span><br/>
-                                    </span>
                                 </center>
                             </p>
                         </td>
