ac51d25d1e10a1ef5885608ab2dfd8af5353c5be
Azimul-Haque <orbachinujbuk@gmail.com>
2022-03-27 02:06:08 +0600

    Commit Sun 03/27/2022  2:06:08.33 9562

--- resources/views/collection/pdf/collectionreceiptgenerate.blade.php
+++ resources/views/collection/pdf/collectionreceiptgenerate.blade.php
@@ -80,7 +80,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <td align="right" style="padding-right: 15px;">রশিদ নং - {{ $studentidcollections[0]->receipt_no }}</td>
+                            <td align="right" style="padding-right: 20px;">রশিদ নং - {{ $studentidcollections[0]->receipt_no }}</td>
                         </tr>
                         <tr>
                             <td style="padding: 15px;">
@@ -356,7 +356,7 @@
                             </td>
                         </tr>
                         <tr>
-                            <td align="right" style="padding-right: 15px;">রশিদ নং - {{ $studentidcollections[0]->receipt_no }}</td>
+                            <td align="right" style="padding-right: 20px;">রশিদ নং - {{ $studentidcollections[0]->receipt_no }}</td>
                         </tr>
                         <tr>
                             <td style="padding: 15px;">
