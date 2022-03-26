5894f298e5dad185ef4d1386fd9889251052d82c
A. H. M. Azimul Haque <orbachinujbuk@gmail.com>
2022-03-26 18:06:27 +0600

    Commit Sat 03/26/2022 18:06:27.26 11383

--- resources/views/collection/pdf/collectionreceiptgenerate.blade.php
+++ resources/views/collection/pdf/collectionreceiptgenerate.blade.php
@@ -35,21 +35,41 @@
                     <tr>
                         <td>
                             <p style="text-align: center; font-size: 20px;">
-                            <center>
-                                <b>{{ Auth::user()->school->name_bangla }}</b><br/>
-                                <span style="font-size: 15px;">
-                                স্থাপিতঃ {{ bangla(Auth::user()->school->established) }} ইংরেজি | ইআইআইএনঃ {{ bangla(Auth::user()->school->eiin) }}<br/>
-                                <span style="font-size: 18px;">
-                                    <b>{{ bangla(date('F d, Y', strtotime($data[0]))) }} - {{ bangla(date('F d, Y', strtotime($data[1]))) }}</b>
-                                </span><br/>
-                                <span style="font-size: 20px;"><u>দৈনিক খতিয়ান</u></span><br/>
-                                </span>
-                            </cen
+                                <center>
+                                    <b>{{ Auth::user()->school->name_bangla }}</b><br/>
+                                    <span style="font-size: 15px;">
+                                    স্থাপিতঃ {{ bangla(Auth::user()->school->established) }} ইংরেজি | ইআইআইএনঃ {{ bangla(Auth::user()->school->eiin) }}<br/>
+                                    <span style="font-size: 18px;">
+                                        <b>{{ bangla(date('F d, Y', strtotime($data[0]))) }} - {{ bangla(date('F d, Y', strtotime($data[1]))) }}</b>
+                                    </span><br/>
+                                    <span style="font-size: 20px;"><u>দৈনিক খতিয়ান</u></span><br/>
+                                    </span>
+                                </center>
+                            </p>
                         </td>
                     </tr>
                 </table>
             </td>
-            <td>2</td>
+            <td>
+                <table>
+                    <tr>
+                        <td>
+                            <p style="text-align: center; font-size: 20px;">
+                                <center>
+                                    <b>{{ Auth::user()->school->name_bangla }}</b><br/>
+                                    <span style="font-size: 15px;">
+                                    স্থাপিতঃ {{ bangla(Auth::user()->school->established) }} ইংরেজি | ইআইআইএনঃ {{ bangla(Auth::user()->school->eiin) }}<br/>
+                                    <span style="font-size: 18px;">
+                                        <b>{{ bangla(date('F d, Y', strtotime($data[0]))) }} - {{ bangla(date('F d, Y', strtotime($data[1]))) }}</b>
+                                    </span><br/>
+                                    <span style="font-size: 20px;"><u>দৈনিক খতিয়ান</u></span><br/>
+                                    </span>
+                                </center>
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
         </tr>
     </table>
     <table>
